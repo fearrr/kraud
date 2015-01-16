85f7c45acf392f5a89445e8e248c5b91154da0e8
dimadima <7235200@gmail.com>
2015-01-16 09:17:25 +0000

    precompile

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,7 @@ module KraudApp
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w(ckeditor/*)
+    config.assets.precompile += %w(bxslider/*)
+    config.assets.precompile += %w(left.png right.png)
   end
 end
