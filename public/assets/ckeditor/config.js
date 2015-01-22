533b48e1ba5f70734a295470bc3cba9d8b30e014
dimadima <7235200@gmail.com>
2015-01-22 12:41:48 +0000

    ckeditor always plain text

--- public/assets/ckeditor/config.js
+++ public/assets/ckeditor/config.js
@@ -14,6 +14,7 @@ CKEDITOR.editorConfig = function (config) {
     // Define changes to default configuration here. For example:
     // config.language = 'fr';
     // config.uiColor = '#AADC6E';
+    config.forcePasteAsPlainText = true;
     config.stylesSet = 'custom';
     /* Filebrowser routes */
     // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
