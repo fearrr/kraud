bca859e3e19daa815aadca7dc7d9e5e16a2a8e0e
dimadima <7235200@gmail.com>
2015-02-09 13:11:00 +0000

    bxslider esize 100% width

--- app/models/slider.rb
+++ app/models/slider.rb
@@ -1,4 +1,4 @@
 class Slider < ActiveRecord::Base
-  has_attached_file :image, :styles => { :normal => "1120x300>", :preview => "200x100>"}, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :normal => "1123x300>", :preview => "200x100>"}, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
