7966222a783fb6da768df904df63704b827290da
Shukshuruk <mkaban4ik@gmail.com>
2015-04-22 12:22:26 +0000

    Work time

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,5 +51,5 @@ Rails.application.configure do
       :authentication => "plain",
       :enable_starttls_auto => true
   }
-  config.consider_all_requests_local = true
+  config.consider_all_requests_local = false
 end
